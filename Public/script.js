6d81300284e9adf3687a6e29b74cfd77159c517a
ASTROSANZ <121183390+ASTROSANZ@users.noreply.github.com>
2024-10-02 23:04:16 -0300

    Add files via upload

---
+++ Public/script.js
@@ -0,0 +1,18 @@
+document.getElementById('paymentForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+
+    fetch('/submit', {
+        method: 'POST',
+        body: new URLSearchParams(formData)
+    })
+    .then(response => response.text())
+    .then(result => {
+        document.getElementById('resultado').textContent = result;
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+        document.getElementById('resultado').textContent = 'Erro ao enviar os dados.';
+    });
+});
