ff9dbc25626a3f539a35b08e9a30924399acd0a7
ASTROSANZ <121183390+ASTROSANZ@users.noreply.github.com>
2024-10-04 20:17:10 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -3,9 +3,18 @@ document.getElementById('paymentForm').addEventListener('submit', function(event
 
     const formData = new FormData(this);
 
+    // Criar um objeto com os dados do formulário
+    const data = {};
+    formData.forEach((value, key) => {
+        data[key] = value;
+    });
+
     fetch('https://recebedor-dados.onrender.com/submit', {
         method: 'POST',
-        body: new URLSearchParams(formData)
+        headers: {
+            'Content-Type': 'application/json' // Definindo o tipo de conteúdo para JSON
+        },
+        body: JSON.stringify(data) // Convertendo os dados para JSON
     })
     .then(response => {
         if (!response.ok) {
